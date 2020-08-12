faf7f0201a854c684025d1869e7a1a188feab427
SKCC <SKCC@SKCC18D00071.SKCC.NET>
2020-08-12 14:06:33 +0900

    first commit

---
+++ src/main/java/local/PolicyHandler.java
@@ -0,0 +1,35 @@
+package local;
+
+import local.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+
+    @Autowired
+    DeliveryRepository deliveryRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverOrdered_Ship(@Payload Ordered ordered){
+
+        // 비동기 호출
+        if(ordered.isMe()){
+            Delivery delivery = new Delivery();
+            delivery.setOrderId(ordered.getId());
+            delivery.setStatus("SHIPPED");
+
+            deliveryRepository.save(delivery);
+        }
+    }
+
+}
